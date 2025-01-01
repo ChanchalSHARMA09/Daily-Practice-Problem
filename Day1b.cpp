27cc8821693667cf51ff2ec2fd6921e13c5ef939
ChanchalSHARMA09 <chanchal.2312051@gmail.com>
2025-01-01 15:32:07 +0530

    DAY 2: 2nd Problem

--- Day1b.cpp
+++ Day1b.cpp
@@ -1,1 +1,15 @@
 //CP PROBLEM 2 : Area Of A Rectangle Or Circle.
+
+#include<iostream>
+using namespace std;
+int main(){
+    int Side_A, Side_B, Radius;
+    cout<<"Enter The Lenght: ";
+    cin>> Side_A;
+    cout<<"Enter The Breath: ";
+    cin>> Side_B;
+    cout<<"Area Of Rectangle: "<< Side_A*Side_B;
+    cout<<"\nEnter The Radius: ";
+    cin>>Radius;
+    cout<<"Area Of Circle: "<< 3.14*Radius*Radius;
+}
