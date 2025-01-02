5146b150e42bad3d38ffe8880cc7727a18aaf10e
ChanchalSHARMA09 <chanchal.2312051@gmail.com>
2025-01-02 19:12:59 +0530

    DAY 2: second Problem

--- Day2b.cpp
+++ Day2b.cpp
@@ -1,1 +1,15 @@
 // CP Problem 2 : Sum of all even numbers up to N.
+
+#include<iostream>
+using namespace std;
+int main(){
+    int N,sum=0,i;
+    cout<<"Enter a Number: ";
+    cin>>N;
+    for(i=0;i<=N;i=i+2){
+        sum=sum+i;
+
+    }
+cout<<"Sum Of N even numbers is: "<<sum;
+
+}
