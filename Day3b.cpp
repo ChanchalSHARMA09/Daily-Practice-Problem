6f931698ad2a3535ca03d32bf52552b1a721cef2
ChanchalSHARMA09 <chanchal.2312051@gmail.com>
2025-01-03 22:46:30 +0530

    DAY 2: second problem

--- Day3b.cpp
+++ Day3b.cpp
@@ -1,1 +1,15 @@
-// CP Problem 2 : Fibonacci series up to N.
+// CP Problem 2 : Factorial using loops.
+
+#include<iostream>
+using namespace std;
+int main(){
+ int N;
+ int num=1;
+ cout<<"Enter the number ";
+ cin>>N;
+ for(int i=1;i<=N;i++){
+ num=num*i;
+ }
+ cout<<num;
+
+}
