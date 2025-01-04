949bda5a364223d3a5b6d534c913584820dd8d49
ChanchalSHARMA09 <chanchal.2312051@gmail.com>
2025-01-05 00:12:02 +0530

    DAY 4: second problem

--- Day4b.cpp
+++ Day4b.cpp
@@ -2,5 +2,15 @@
 #include<iostream>
 using namespace std;
 int main(){
-    
+    int arr[5];
+      int sum=0;
+    for(int i=0;i<5;i++){
+        cin>>arr[i];
+    }
+    for(int i=0;i<5;i++){
+        sum=sum+arr[i];
+  
+    }
+  
+         cout<<sum;
 }
