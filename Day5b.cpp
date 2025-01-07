28a6496537556a67992157469f86b2ece0e539c4
ChanchalSHARMA09 <chanchal.2312051@gmail.com>
2025-01-07 19:33:07 +0530

    day 7

--- Day5b.cpp
+++ Day5b.cpp
@@ -1,8 +1,16 @@
-// CP Problem 2 : Count vowels in a string.
+// CP Problem 2 : reverse a number
+
 
 #include<iostream>
 using namespace std;
 int main(){
-    cout<<"hw";
+    int num,rev=0;
+    cout<<"enter number:";
+    cin>>num;
+    while(num>0){
+        rev=rev*10+num%10;
+        num=num/10;
+    }
+    cout<<rev;
     return 0;
 }
